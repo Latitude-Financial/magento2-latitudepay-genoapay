06841a81caef9e3f1da66fe74140f928e3816f20
hieptran87 <63086258+hieptran87@users.noreply.github.com>
2022-01-20 11:41:44 +0700

    Fix phpcs

--- bin/phpcs.sh
+++ bin/phpcs.sh
@@ -3,5 +3,5 @@ echo "PHP Code Sniffer with Magento Extension Quality Program"
 docker-compose exec magento \
 	phpcs --config-set show_warnings 0
 docker-compose exec magento \
-	phpcs --standard=Magento2 --extensions=php --ignore=*/vendor/,*/Tests/ --colors -s -p -v /var/www/html/app/code/Latitude/Payment
+	phpcs --standard=Magento2 --exclude=PHPCompatibility --extensions=php --ignore=*/vendor/,*/Tests/ --colors -s -p -v /var/www/html/app/code/Latitude/Payment
 	$*
